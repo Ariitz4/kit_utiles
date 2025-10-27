87ba81f262aad2fe08e0fafe6fe3471b198fd42a
amaiaDi <amaia.di@icjardin.com>
2025-10-27 16:27:52 +0100

    Cambios en tes1 para crear conflicto con remoto

--- tests/test_e01_sum_list.py
+++ tests/test_e01_sum_list.py
@@ -1,6 +1,7 @@
 from src.numbers import sum_list
 import pytest
 
+#TODO - Quitar el comentario para la fase 3 del proyecto
 def test_sum_list_with_numbers():
     # Arrange
     numbers = [1, 2, 3, 4]
